6708aa48ad69dd67646ad3b3b294c5ce438e5fa7
anthony-duque <adduxe@hotmail.com>
2023-03-25 22:29:46 -0700

    Added a generic json search function but is still not used in the display

--- scripts/PatientSearch.js
+++ scripts/PatientSearch.js
@@ -5,6 +5,32 @@ var patientSearchCtrlr = function($scope){
 
    $scope.patientCount = 15;
    $scope.patientList = patientList;
+
+      // function that looks for a value in a list
+   function search(source, searchField, searchValue) {
+
+      var results = [];
+
+      for (var i=0 ; i < source.list.length ; i++)
+      {
+          if (source.list[i][searchField] == searchVal) {
+              results.push(source.list[i]);
+          }
+      }
+
+      /// equivalent forEach version
+      /*
+      source forEach((record) => {
+         if(record[searchField] == searchVal){
+            results.push(record);
+         }
+
+      });
+      */
+
+       return results;
+   }   // function search()
+
 }
 
 PatientSearch.controller("patientSearchController", patientSearchCtrlr);
