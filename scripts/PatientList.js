2f1dc26309639a88d9ac379982ef5d4a1c4aac1c
anthony-duque <adduxe@hotmail.com>
2023-03-25 22:02:49 -0700

    Added a temporary patient list json code to test the display

---
+++ scripts/PatientList.js
@@ -0,0 +1,23 @@
+var patientList = [
+      {
+         MRN: "ABC123456789DEF",
+         FirstName: "James",
+         LastName: "Bond",
+         Gender: "Male",
+         BirthDate: new Date("10/22/1969")
+      },
+      {
+         MRN: "DEF098765432GHI",
+         FirstName: "Edward",
+         LastName: "Scissorhands",
+         Gender: "Male",
+         BirthDate: new Date("01/14/1971")
+      },
+      {
+         MRN: "XYZ098765432ABC",
+         FirstName: "Jennifer",
+         LastName: "Aniston",
+         Gender: "Female",
+         BirthDate: new Date("03/15/1990")
+      }
+];
--- scripts/PatientSearch.js
+++ scripts/PatientSearch.js
@@ -4,6 +4,7 @@ var PatientSearch = angular.module("patientSearchModule", []);
 var patientSearchCtrlr = function($scope){
 
    $scope.patientCount = 15;
+   $scope.patientList = patientList;
 }
 
 PatientSearch.controller("patientSearchController", patientSearchCtrlr);
