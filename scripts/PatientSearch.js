52fe7b0c1b85395ffc6192b60e1167fb7e0d68e3
anthony-duque <adduxe@hotmail.com>
2023-03-28 16:10:04 -0700

    Added Angular search for Patients in Patient Search page

--- scripts/PatientSearch.js
+++ scripts/PatientSearch.js
@@ -28,31 +28,6 @@ var patientSearchCtrlr = function($scope){
       $scope.sortField = $scope.sortOrder + sortBy;
    }  // SortPatients()
 
-      // function that looks for a value in a list
-   function search(source, searchField, searchValue) {
-
-      var results = [];
-
-      for (var i=0 ; i < source.list.length ; i++)
-      {
-          if (source.list[i][searchField] == searchVal) {
-              results.push(source.list[i]);
-          }
-      }
-
-      /// equivalent forEach version
-      /*
-      source forEach((record) => {
-         if(record[searchField] == searchVal){
-            results.push(record);
-         }
-
-      });
-      */
-
-       return results;
-   }   // function search()
-
-}
+}  // patientSearchCtrlr()
 
 PatientSearch.controller("patientSearchController", patientSearchCtrlr);
